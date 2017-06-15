549c426bb519d8408c41f55feeb86b4457f50b4f
Anushri <anushree.74@gmail.com>
2017-06-15 11:18:20 +0530

    Updated DBConfig

--- laptoptreasurebackend/src/main/java/com/niit/laptoptreasurebackend/config/DBConfig.java
+++ laptoptreasurebackend/src/main/java/com/niit/laptoptreasurebackend/config/DBConfig.java
@@ -5,6 +5,7 @@ import java.util.Properties;
 import javax.sql.DataSource;
 
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -12,45 +13,64 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
 
+import com.niit.laptoptreasurebackend.dao.CategoryDAO;
+import com.niit.laptoptreasurebackend.model.Category;
+
 
 
 @Configuration
 @ComponentScan("com.niit.laptoptreasurebackend")
 
 public class DBConfig
-{
+{      @Autowired
 	   @Bean(name="dataSource")
-	  public DataSource getDataSource()
+	  public DataSource getH2DataSource()
 	  {
-
+           System.out.println("----Data Source Creation-----");
 		   DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		   dataSource.setDriverClassName("org.h2.Driver");
 		   dataSource.setUrl("jdbc:h2:~/database");
 		   dataSource.setUsername("sa");
 		   dataSource.setPassword("anuash02");
+		   System.out.println("----Data Source Creation-----");
 		   return dataSource;
 	  }
-	   
+	   @Autowired
 	   @Bean(name="sessionFactory")
-	   public SessionFactory getSessionFactory()
+	   public SessionFactory getSessionFactory(DataSource dataSource)
 	   {
-
+           System.out.println("------Hibernate properties creation------");
 		   Properties properties=new Properties();
 		   properties.setProperty("hibernate.hbm2ddl.auto", "update");
 		   properties.put("hibernate.show_sql", "true");
 		   properties.put("hibernate.dialect", "org.hibernate.dialect.h2dialect");
+		   System.out.println("------Hibernate properties created------");
 		   
+		   System.out.println("------LacalSessionFactoryBuider object creation------");
+		   LocalSessionFactoryBuilder sessionBuilder=new LocalSessionFactoryBuilder(dataSource);
+		   sessionBuilder.addProperties(properties);
+		   System.out.println("------Factory Builder object created------");
+		   sessionBuilder.addAnnotatedClass(Category.class);
 		   
-		   LocalSessionFactoryBuilder sessionBuilder=new LocalSessionFactoryBuilder(getDataSource());
-		   sessionBuilder.setProperties(properties);
+		   System.out.println("------Session Factory  object creation------");
+		   SessionFactory sessionFactory=sessionBuilder.buildSessionFactory();
+		   System.out.println("------Session factory object created------");
+		   //return sessionFactory;
 		   return sessionBuilder.buildSessionFactory();
 	   }
-       
-	   @Bean(name="transManager")
+       @Autowired
+	   @Bean(name="txManager")
 	   public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory)
 	   {
 
 		   HibernateTransactionManager transactionManager=new HibernateTransactionManager(sessionFactory);
 				   return transactionManager;
+	   }
+	   @Autowired
+	   @Bean(name="categoryDAO")
+	   public CategoryDAO getCategoryDAO(SessionFactory sessionFactory)
+	   {
+
+		   return new CategoryDAO(sessionFactory);
 	   }
 }
--- laptoptreasurebackend/src/main/java/com/niit/laptoptreasurebackend/dao/CategoryDAO.java
+++ laptoptreasurebackend/src/main/java/com/niit/laptoptreasurebackend/dao/CategoryDAO.java
@@ -3,6 +3,10 @@ package com.niit.laptoptreasurebackend.dao;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.niit.laptoptreasurebackend.model.*;
+
+
+
 public class CategoryDAO 
 {
 	@Autowired
@@ -11,5 +15,10 @@ public class CategoryDAO
 	public CategoryDAO(SessionFactory sessionFactory)
 	{
        this.sessionFactory=sessionFactory;
+	}
+	
+	public void insertCategory(Category category)
+	{
+		sessionFactory.getCurrentSession().saveOrUpdate(category);
 	}
 }
